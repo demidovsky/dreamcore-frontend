ff67fe14bb5ff01b0922f419f87d7f823c33daac
Dmitry Demidovsky <d.demidovsky@gmail.com>
2019-01-08 11:44:35 +0300

    Form includes image

--- src/components/_modules/Achievements/edit.jsx
+++ src/components/_modules/Achievements/edit.jsx
@@ -15,25 +15,16 @@ class AchievementEdit extends Component {
   }
 
   save = (values, { setSubmitting }) => {
-    if (this.state.id) {
-      axios.patch(`http://localhost:1337/achievements/${ this.state.id }`, {
-        name: values.name,
-        notes: values.notes,
-      })
-      .then((response) => {
-         console.log(response);
-         setSubmitting(false);
-      });
-    } else {
-      axios.post('http://localhost:1337/achievements', {
-        name: values.name,
-        notes: values.notes,
-      })
-      .then((response) => {
-         console.log(response);
-         setSubmitting(false);
-      });
-    }
+    const method = this.state.id ? 'patch' : 'post';
+    const url = `http://localhost:1337/achievements/${ this.state.id || '' }`;
+
+    axios[method](url, {
+      name: values.name,
+      notes: values.notes,
+    })
+    .then((response) => {
+      setTimeout(() => {console.log(response); setSubmitting(false);}, 3000);
+    });
   }
 
   componentDidMount () {
@@ -69,135 +60,108 @@ class AchievementEdit extends Component {
 
     return (
       <React.Fragment>
-        <PageHeader breadcrumps={ ['Modules', 'Achievements', 'Create achievement'] } />
+        <PageHeader breadcrumps={ ['Modules', 'Achievements', `${ id ? 'Edit' : 'Create' } achievement`] } />
 
-        <div className="row">
-        <div className="col-sm-6 col-lg-6">
-          <div className="card create-achievement">
-            <h3 className="card-header">Description</h3>
-            <div className="card-body">
+        <Formik enableReinitialize initialValues={ { name, notes } } onSubmit={ this.save }>
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, }) => (
+            <form onSubmit={ handleSubmit }>
+              <div className="row">
+                <div className="col-sm-6 col-lg-6">
+                  <div className="card create-achievement">
+                    <h3 className="card-header">Description</h3>
+                    <div className="card-body">
 
-<Formik enableReinitialize initialValues={ { name, notes } } onSubmit={ this.save }>
-{({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
+                      <div className="form-group">
+                        <label htmlFor="inputText3" className="col-form-label">Name</label>
+                        <input name="name" type="text" className="form-control"
+                      value={ values.name } maxLength="64" onChange={ handleChange } onBlur={ handleBlur } />
+                      </div>
 
+                      <div className="form-group">
+                        <label htmlFor="exampleFormControlTextarea1">Notes</label>
+                        <textarea name="notes" className="form-control" rows="3"
+                      value={ values.notes } onChange={ handleChange } onBlur={ handleBlur }></textarea>
+                      </div>
 
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-4">
-                    <label className="col-form-label">Preview</label>
-                    <div className="card card-figure">
-                      <figure className="figure">
-                        <img className="img-fluid" src={ imageUrl || noImage } alt="" />
-                        {/*<figcaption className="figure-caption">
-                    <h6 className="figure-title"> </h6>
-                  </figcaption>*/}
-                      </figure>
-                    </div>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="form-group">
-                      <label htmlFor="inputText3" className="col-form-label">Name</label>
-                      <input name="name" type="text" className="form-control"
-                        value={ values.name } onChange={handleChange} onBlur={handleBlur} />
+                      <label className="col-form-label">Preview</label>
+                      <div className="card card-figure achievement-preview">
+                        <figure className="figure">
+                          <img className="img-fluid" src={ imageUrl || noImage } alt="" />
+                          <figcaption className="figure-caption">
+                            <h6 className="figure-title">{ values.name }</h6>
+                          </figcaption>
+                        </figure>
+                      </div>
+
+                      <div className="form-group">
+                        <button disabled={ isSubmitting } type="submit" className="btn btn-lg btn-success">Save</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-            
-                <div className="form-group">
-                  <label htmlFor="exampleFormControlTextarea1">Notes</label>
-                  <textarea name="notes" className="form-control" rows="3"
-                    value={ values.notes } onChange={handleChange} onBlur={handleBlur}></textarea>
-                </div>
 
-                <div className="form-group">
-                  <button type="submit" className="btn btn-lg btn-success">Save</button>
-                </div>
+                <div className="col-sm-6 col-lg-6">
+                  <div className="card">
+                    <h3 className="card-header">Image</h3>
+                    <div className="card-body border-top">
 
-              </form>
-              )}
-    </Formik>
-            </div>
-          </div>
-        </div>
+                      <FileInput/>
 
-        <div className="col-sm-6 col-lg-6">
-          <div className="card">
-            <h3 className="card-header">Image</h3>
-            <div className="card-body border-top">
-              {/*<form>*/}
+                      <div className="row divider-with-text">
+                        <div className="col"><hr/></div>
+                        <div className="col-auto">or</div>
+                        <div className="col"><hr/></div>
+                      </div>
 
-                <FileInput/>
-                {/*<div className="form-group custom-file">
-                  <input type="file" className="custom-file-input" id="customFile"/>
-                  <button className="btn btn-outline-primary btn-xs">Select</button>
-                  <label className="custom-file-label" htmlFor="customFile">Upload...</label>
-                </div>*/}
+                      <div className="form-group">
+                        <label htmlFor="inputSmall" className="col-form-label">Paste URL</label>
+                        <input onChange={ handleChange } type="text" value="" className="form-control form-control-sm"/>
+                      </div>
 
-                <div className="row divider-with-text">
-                  <div className="col"><hr/></div>
-                  <div className="col-auto">or</div>
-                  <div className="col"><hr/></div>
-                </div>
+                      <div className="row divider-with-text">
+                        <div className="col"><hr/></div>
+                        <div className="col-auto">or</div>
+                        <div className="col"><hr/></div>
+                      </div>
 
-                <div className="form-group">
-                  <label htmlFor="inputSmall" className="col-form-label">Paste URL</label>
-                  <input id="inputSmall" type="text" value="" className="form-control form-control-sm"/>
-                </div>
+                      <div className="form-group">
+                        <label htmlFor="inputSmall" className="col-form-label">Find by keywords</label>
+                        <input type="text" value="" className="form-control form-control-sm"/>
+                      </div>
 
-                <div className="row divider-with-text">
-                  <div className="col"><hr/></div>
-                  <div className="col-auto">or</div>
-                  <div className="col"><hr/></div>
-                </div>
+                      <div className="mb-2">
 
-                <div className="form-group">
-                  <label htmlFor="inputSmall" className="col-form-label">Find by keywords</label>
-                  <input id="inputSmall" type="text" value="" className="form-control form-control-sm"/>
-                </div>
+                        <div className="img-thumb">
+                          <img className="img-fluid" alt="" src="http://c1.staticflickr.com/5/4071/4277029894_7023f3ca2f_m.jpg"/>
+                        </div>
+                        <div className="img-thumb">
+                          <img className="img-fluid" alt="" src="http://c1.staticflickr.com/8/7065/6852957177_9f27f30f12_m.jpg"/>
+                        </div>
+                        <div className="img-thumb">
+                          <img className="img-fluid" alt="" src="http://c1.staticflickr.com/4/3936/14950097294_6212158ff5_m.jpg"/>
+                        </div>
 
-                <div className="mb-2">
+                        <div className="img-thumb">
+                          <img className="img-fluid" alt="" src="http://c1.staticflickr.com/9/8420/8710133086_936fe32e0f_m.jpg"/>
+                        </div>
+                        <div className="img-thumb">
+                          <img className="img-fluid" alt="" src="http://c1.staticflickr.com/9/8206/8235791752_c26a81fa90_m.jpg"/>
+                        </div>
 
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/5/4071/4277029894_7023f3ca2f_m.jpg"/>
+                      </div>
+
+                      <div className="form-group">
+                        <button className="btn btn-outline-primary">Load more...</button>
+                      </div>
+             
+                    </div>
                   </div>
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/8/7065/6852957177_9f27f30f12_m.jpg"/>
-                  </div>
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/4/3936/14950097294_6212158ff5_m.jpg"/>
-                  </div>
-
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/9/8420/8710133086_936fe32e0f_m.jpg"/>
-                  </div>
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/9/8206/8235791752_c26a81fa90_m.jpg"/>
-                  </div>
-                  <div className="img-thumb">
-                    <img className="img-fluid" alt="" src="http://c1.staticflickr.com/8/7032/6654543769_08f14bb71b_m.jpg"/>
-                  </div>
-
                 </div>
 
-                <div className="form-group">
-                  <button className="btn btn-outline-primary">Load more...</button>
-                </div>
-           
-              {/*</form>*/}
-            </div>
-          </div>
-        </div>
+              </div>
+            </form>)}
+        </Formik>
 
-      </div>
       </React.Fragment>)
   }
 }
