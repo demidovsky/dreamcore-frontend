bf6a828a99d33706ac1e26803ff662a4eaf9cdc9
Dmitry Demidovsky <d.demidovsky@gmail.com>
2019-02-09 23:57:02 +0300

    Fixed icon font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './material-design-iconic-font/css/materialdesignicons.min.css';
 import './style.css';
 import './index.css';
 import App from './App';
