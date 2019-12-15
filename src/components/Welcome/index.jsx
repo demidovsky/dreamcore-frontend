01b4c82fecde105776d875f8b7a0e6ee7d4d41c9
Dmitry Demidovsky <d.demidovsky@gmail.com>
2019-12-15 13:59:16 +0300

    Updated Welcome

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
-import achievements_screenshot from './achievements_screenshot.jpg';
-import friends_screenshot from './friends_screenshot.jpg';
+import intro1 from './dreamcore_intro/1.jpeg';
+import intro2 from './dreamcore_intro/2.jpeg';
+import intro3 from './dreamcore_intro/3.jpeg';
+import intro4 from './dreamcore_intro/4.jpeg';
 
 const Welcome = () => (
-  <Fragment>
-    <h2>Привет! Это альфа-версия Dreamcore</h2>
+  <div>
+    <h2>Привет! Это Dreamcore, версия 0.1</h2>
     <p className="lead">
       Что тут сейчас можно делать?
     </p>
@@ -13,46 +15,83 @@ const Welcome = () => (
       <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
         <div className="card">
           <div className="card-body">
-            <h3 className="card-title mb-2">Создавать и выполнять ачивменты</h3>
+            <h3 className="card-title mb-2">1. Придумывать цели</h3>
             <p className="card-text">
-              От (англ.) <i>achievement</i> &mdash; "достижение"; термин, уже устоявшийся
-              за долгую историю компьютерных игр.
+              Или, точнее, <b>ачивменты</b>.
             </p>
-            <p>
-              Это некая цель, как правило, не являющаяся необходимой или критической, однако интересная или приятная для выполнения.
+            <p className="card-text">
+              За долгую историю компьютерных игр устоялось понятие ачивмента,
+              как <b>цели,</b> обычно <b>не являющейся необходимой</b> или 
+              критической, <b>однако интересной</b> или приятной для выполнения.
             </p>
             {/*<p>
               Ачивменты &mdash; это способ принести геймификацию в реальную жизнь.
             </p>*/}
-            <a href="/app/achievements" className="btn btn-primary">Перейти к модулю Achievements</a>
             {/*<p className="text-muted">Last updated 3 mins ago</p>*/}
           </div>
-          <img className="img-fluid" src={ achievements_screenshot } alt="Card image cap"/>
+          <img className="img-fluid" src={ intro1 } alt="Card image cap"/>
         </div>
       </div>
 
       <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
         <div className="card">
           <div className="card-body">
-            <h3 className="card-title mb-2">Добавлять друзей и делиться с ними</h3>
+            <h3 className="card-title mb-2">2. Задавать им названия</h3>
             <p className="card-text">
-              Так можно найти общие мечты, о которых вы даже не догадывались!
-              И вместе приступить к их реализации. 
+              Ну ничего себе, да? Но вообще-то они используются, например, при подборе визуализации.
             </p>
             <p className="card-text">
-              Изначально ваши ачивменты видны только вам.
-              Вы самостоятельно выбираете, какие из них сделать доступными для друзей.
+              Лучше всего использовать короткие титулы, отвечающие на вопрос 
+              &nbsp;<b>&quot;Кто ты, когда заработаешь этот ачивмент?&quot;</b>&nbsp;
+              Например, &quot;Coffee Drinker&quot; или &quot;Marathon Runner&quot;. <i> Иначе&hellip;</i>
             </p>
-            <a href="/app/friends" className="btn btn-primary">Перейти к модулю Friends</a>
             {/*<p className="text-muted">Last updated 3 mins ago</p>*/}
           </div>
-          <img className="img-fluid" src={ friends_screenshot } alt="Card image cap"/>
+          <img className="img-fluid" src={ intro2 } alt="Card image cap"/>
+        </div>
+      </div>
+
+      <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div className="card">
+          <div className="card-body">
+            <h3 className="card-title mb-2">3. Визуализировать их </h3>
+            <p className="card-text">
+              &hellip;иначе FLickr умеет доставлять за счет картинок, подходящих к цели чуть менее, чем никак. 
+              Если серьезно, <b>можно загрузить ещё вариантов, либо поменять ключевые слова</b>.
+            </p>
+            <p>
+              Конечно же, можно присвоить цели картинку из файла или просто по ссылке, но это не так весело.
+            </p>
+            {/*<p>
+              Ачивменты &mdash; это способ принести геймификацию в реальную жизнь.
+            </p>*/}
+            {/*<p className="text-muted">Last updated 3 mins ago</p>*/}
+          </div>
+          <img className="img-fluid" src={ intro3 } alt="Card image cap"/>
+        </div>
+      </div>
+
+      <div className="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div className="card">
+          <div className="card-body">
+            <h3 className="card-title mb-2">Готово!</h3>
+            <p className="card-text">
+              <b>Сохрани</b> ачивмент и создавай следующий!
+            </p>
+            <p className="card-text">
+              Изначально <b>твои ачивменты видны только тебе</b>.
+              Впоследствии можно будет выбрать, какие из них сделать доступными для друзей.
+            </p>
+            <a href="/app/achievements" className="btn btn-primary">Круто, погнали!</a>
+            {/*<p className="text-muted">Last updated 3 mins ago</p>*/}
+          </div>
+          <img className="img-fluid" src={ intro4 } alt="Card image cap"/>
         </div>
       </div>
 
     </div>
 
-  </Fragment>
+  </div>
 );
 
 export default Welcome;
