f8f9f3b4f2a7b9240d0c5f1f98259460aac260b2
Dmitry Demidovsky <d.demidovsky@gmail.com>
2019-12-16 15:33:50 +0300

    Unregister service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './material-design-iconic-font/css/materialdesignicons.min.css';
 import './style.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { registerServiceWorker, unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+unregister();
